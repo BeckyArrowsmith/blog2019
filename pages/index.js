63b2b4779a8e869fdb2ae0918fa71101abc146d3
Becky Arrowsmith <becky.arrowsmith@googlemail.com>
2019-08-31 19:19:11 +0100

    Rendering something from cntful json blob

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
+import data from "../data/contentful-data.json";
+
 function Home() {
-    return <div>Hello, world!</div>
-}
+    // return JSON.stringify(data[0].fields.title + data[0].fields.content);
+    const postData = data.map((post) => 
+        <article id={post.fields.slug}>
+            <h2>{post.fields.title}</h2>
+            <p>{post.fields.content}</p>
+        </article>);
+
+    return (
+        <div>
+            {postData}
+        </div>
+    );
+
+    // const data = [{ "name": "test1", "content": "content" }, { "name": "test2", "content": "content" }];
+    // const listItems = data.map((d) => <section>{d.name} {d.content}</section>);
+
+    // return (
+    //     <div>
+    //         {listItems}
+    //     </div>
+    // );
+}   
 
 export default Home
