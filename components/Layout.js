5bcf37ebb43449bf3931a59903c703200e44b954
Becky Arrowsmith <becky.arrowsmith@googlemail.com>
2019-09-30 15:35:20 +0100

    Update layout to have some global jsx so _next sits right

--- components/Layout.js
+++ components/Layout.js
@@ -16,7 +16,28 @@ const Layout = props => (
 
     <Footer />
 
-    <style jsx>{`
+    <style jsx global>{`
+      * {
+        box-sizing: border-box;
+      }
+
+      html,
+      body,
+      #__next {
+        height: 100%;
+        width: 100%;
+      }
+
+      body {
+        margin: 0;
+        padding: 0;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+          "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+          "Helvetica Neue", sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+      }
+      
       .Layout {
         display: flex;
         flex-direction: column;
