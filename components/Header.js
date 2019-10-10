8adf64749ff0f3e55167287e93f3e46d60c0c1ab
Becky Arrowsmith <becky.arrowsmith@googlemail.com>
2019-10-10 19:55:22 +0100

    Remove CSS in JS; add reset and some foundation styles

--- components/Header.js
+++ components/Header.js
@@ -2,10 +2,13 @@ import Nav from '../components/Nav';
 
 const Header = () => (
     <header className="Header">
-        <h1>becky.codes</h1>
-
         <Nav />
 
+        <div class="container">
+            <h1>becky.codes</h1>
+
+            <p>Silly twat who likes computers but not writing blog posts tbh</p>
+        </div>
     </header>
 );
 
--- layouts/Layout.js
+++ layouts/Layout.js
@@ -18,43 +18,6 @@ const Layout = props => (
 
     <Footer />
 
-    <style jsx global>{`
-      * {
-        box-sizing: border-box;
-      }
-
-      html,
-      body,
-      #__next {
-        height: 100%;
-        width: 100%;
-      }
-
-      body {
-        margin: 0;
-        padding: 0;
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
-          "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
-          "Helvetica Neue", sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-      }
-      
-      .Layout {
-        display: flex;
-        flex-direction: column;
-        height: 100%;
-        width: 100%;
-        background: blue;
-      }
-
-      .Content {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        background: yellow;
-      }
-    `}</style>
   </div>
 );
 
