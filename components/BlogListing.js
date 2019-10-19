fa51421f853f4071d7f3fe3e0d51910a4e650b66
Becky Arrowsmith <becky.arrowsmith@googlemail.com>
2019-10-19 13:39:46 +0100

    Add headline images

--- components/BlogListing.js
+++ components/BlogListing.js
@@ -4,26 +4,29 @@ import Markdown from 'markdown-to-jsx';
 function BlogListing() {
     const postData = data.map((post) =>        
         <article key={post.id} id={post.fields.slug}>
-            <h2><a href={'http://localhost:3000/post?slug=' + post.fields.slug}>{post.fields.title}</a></h2>
+            <section class="headline-image">
+                <img src={post.fields.headlineImage.fields.file.url} alt={post.fields.headlineImage.fields.description} />
+            </section>
 
-            <p>Created at: {post.sys.createdAt}</p>
-            <p>Updated at: {post.sys.updatedAt}</p>
+            <section class="container">
+                <h2 ><a href={'http://localhost:3000/post?slug=' + post.fields.slug}>{post.fields.title}</a></h2>
+                <p>Created at: {post.sys.createdAt}</p>
+                <p>Updated at: {post.sys.updatedAt}</p>
 
-            <img src={post.fields.headlineImage.fields.file.url} alt={post.fields.headlineImage.fields.description} />
+                <Markdown>{post.fields.content}</Markdown>
 
-            <Markdown>{post.fields.content}</Markdown>
-
-            <section id="tags">
-                <h3>Tags:</h3>
-                <ul>
-                    {post.fields.tags.map((tag) => <li key={post.id}><a href={'http://localhost:3000/tags?tag=' + tag}>{tag}</a></li>)}
-                </ul>
+                <section id="tags">
+                    <h3>Tags:</h3>
+                    <ul>
+                        {post.fields.tags.map((tag) => <li><a href={'http://localhost:3000/tags?tag=' + tag}>{tag}</a></li>)}
+                    </ul>
+                </section>
             </section>
         </article>
     );
 
     return (
-        <div class="container">
+        <div>
             {postData}
         </div>
     );
--- components/BlogPost.js
+++ components/BlogPost.js
@@ -7,26 +7,31 @@ const Post = () => {
     const router = useRouter()
 
     const postData = data.filter(post => post.fields.slug === router.query.slug).map((post) =>
-        <article id={post.fields.slug}>
-            <img src={post.fields.headlineImage.fields.file.url} alt={post.fields.headlineImage.fields.description} />
-            <h2><a href={'http://localhost:3000/post?slug=' + post.fields.slug}>{post.fields.title}</a></h2>
-            <p>Created at: {post.sys.createdAt}</p>
-            <p>Updated at: {post.sys.updatedAt}</p>
+        <article key={post.id} id={post.fields.slug}>
+            <section class="headline-image">
+                <img src={post.fields.headlineImage.fields.file.url} alt={post.fields.headlineImage.fields.description} />
+            </section>
+            
+            <section class="container">
+                <h2 ><a href={'http://localhost:3000/post?slug=' + post.fields.slug}>{post.fields.title}</a></h2>
+                <p>Created at: {post.sys.createdAt}</p>
+                <p>Updated at: {post.sys.updatedAt}</p>
 
-            <Markdown>{post.fields.content}</Markdown>
+                <Markdown>{post.fields.content}</Markdown>
 
-            <section id="tags">
-                <h3>Tags:</h3>
-                <ul>
-                    {post.fields.tags.map((tag) => <li><a href={'http://localhost:3000/tags?tag=' + tag}>{tag}</a></li>)}
-                </ul>
+                <section id="tags">
+                    <h3>Tags:</h3>
+                    <ul>
+                        {post.fields.tags.map((tag) => <li><a href={'http://localhost:3000/tags?tag=' + tag}>{tag}</a></li>)}
+                    </ul>
+                </section>
             </section>
         </article>
     );
     return (
-        <article class="container">
+        <div>
             {postData}
-        </article>
+        </div>
     )
 }
 
