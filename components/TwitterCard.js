e698bad1011c36469192afed9ac6b6e3acf2bacf
Becky Arrowsmith <becky.arrowsmith@googlemail.com>
2019-10-29 20:21:34 +0000

    Fix twitter card url path

--- components/TwitterCard.js
+++ components/TwitterCard.js
@@ -8,14 +8,14 @@ export default class TwitterCard extends React.Component {
                 <Helmet>
                     <meta name="description" content="Personal blog of Becky Arrowsmith. Software Engineer, Tinkerer, Curious Human"/>
                     
-                    <meta name="og:url" content={this.props.url} />
+                    <meta name="og:url" content={"http://becky.codes/post?slug=" + this.props.url} />
                     <meta name="og:title" content={this.props.title} />
                     <meta name="og:description" content={this.props.description} />
                     <meta name="og:image" content={this.props.image} />
                     <meta name="og:type" content="website" />
 
                     <meta name="twitter:card" content="summary_large_image" />
-                    <meta name="twitter:url" contentt={"http://becky.codes/post?slug=/" + this.props.url}/>
+                    <meta name="twitter:url" contentt={"http://becky.codes/post?slug=" + this.props.url}/>
                     <meta name="twitter:title" content={this.props.title}/>
                     <meta name="twitter:description" content={this.props.description}/>
                     <meta name="twitter:image" content={this.props.image}/>
