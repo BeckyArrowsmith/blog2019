c658ece655f66e899020d28ae31fe94203e140c4
Becky Arrowsmith <becky.arrowsmith@googlemail.com>
2019-10-29 19:29:25 +0000

    Fix twitter embeds

--- layouts/Layout.js
+++ layouts/Layout.js
@@ -14,6 +14,7 @@ const Layout = props => (
       <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png"/>
       <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png"/>
       <link rel="manifest" href="/static/site.webmanifest"/>
+      <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
     </Head>
 
     <Header />
