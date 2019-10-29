f3ef5d94722292dadbbcb000b69d59a585fc9715
Becky Arrowsmith <becky.arrowsmith@googlemail.com>
2019-10-29 20:10:20 +0000

    Revert "Try simple twitter meta"
    This reverts commit 10a69e4807febe54d354e45da8bf39e5b5759570.

--- components/TwitterCard.js
+++ components/TwitterCard.js
@@ -7,20 +7,20 @@ export default class TwitterCard extends React.Component {
             <React.Fragment>
                 <Helmet>
                     <meta name="description" content="Personal blog of Becky Arrowsmith. Software Engineer, Tinkerer, Curious Human"/>
-
+                    
                     <meta name="og:url" content={this.props.url} />
                     <meta name="og:title" content={this.props.title} />
                     <meta name="og:description" content={this.props.description} />
                     <meta name="og:image" content={this.props.image} />
                     <meta name="og:type" content="website" />
 
-                    <meta name="twitter:card" content="summary" />
-                    <meta name="twitter:url" content={"http://becky.codes/post?slug=/" + this.props.url}/>
+                    <meta name="twitter:card" content="summary_large_image" />
+                    <meta name="twitter:url" contentt={"http://becky.codes/post?slug=/" + this.props.url}/>
                     <meta name="twitter:title" content={this.props.title}/>
-                    {/* <meta name="twitter:description" content={this.props.description}/>
+                    <meta name="twitter:description" content={this.props.description}/>
                     <meta name="twitter:image" content={this.props.image}/>
                     <meta name="twitter:site" content="@beckycodes" />
-                    <meta name="twitter:creator" content="@beckycodes" /> */}
+                    <meta name="twitter:creator" content="@beckycodes" />
                 </Helmet>
             </React.Fragment>
         );
