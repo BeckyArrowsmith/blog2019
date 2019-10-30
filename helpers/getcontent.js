e77bbdde0420792967f4aec827c1e4844ef87012
BeckyArrowsmith <becky.arrowsmith@googlemail.com>
2019-10-30 20:01:40 +0000

    Tidying: run Prettier on commit

--- helpers/getcontent.js
+++ helpers/getcontent.js
@@ -7,24 +7,23 @@ const SPACE_ID = process.env.CONTENTFUL_SPACE_ID;
 const ACCESS_TOKEN = process.env.CONTENTFUL_CDA_TOKEN;
 
 const client = contentful.createClient({
-  space: SPACE_ID,
-  accessToken: ACCESS_TOKEN
+		space: SPACE_ID,
+		accessToken: ACCESS_TOKEN
 });
 
-client.getEntries({
-    content_type: "blogPost"
-  }).then(function(entries) {
-  let response = JSON.stringify(entries.items);
+client.getEntries({content_type: "blogPost"})
+.then(function(entries) {
+	let response = JSON.stringify(entries.items);
 
-  fs.writeFile("./data/contentful-posts.json", response, error => {
-    if (error) throw error;
-  });
+	fs.writeFile("./data/contentful-posts.json", response, error => {
+		if (error) throw error;
+	});
 });
 
 client.getEntry("32Ua6DxhAO9eUmgLjKVHJ7").then(function(entry) {
-  let response = JSON.stringify(entry);
+	let response = JSON.stringify(entry);
 
-  fs.writeFile("./data/about-page.json", "[" + response + "]", error => {
-    if (error) throw error;
-  });
+	fs.writeFile("./data/about-page.json", "[" + response + "]", error => {
+	if (error) throw error;
+	});
 });
