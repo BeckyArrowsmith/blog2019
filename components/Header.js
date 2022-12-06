a6bee36135906e3c8210bf3eb8365efb697f326d
Becky Arrowsmith <becky.arrowsmith@googlemail.com>
2022-12-06 16:31:33 +0000

    Update blurb

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,8 @@ const Header = () => (
         <div className="container bio">
             <h1>becky.codes</h1>
 
-            <p>Less code these days, and more adventures in enabling the best out of people and engineering teams</p>
+            <p>Less code these days.</p>
+            <p>Adventures in enabling the best out of people and engineering teams.</p>
             <p><a href="http://twitter.com/beckycodes">Twitter</a> <a href="http://github.com/beckyarrowsmith">Github</a></p>
         </div>
     </header>
