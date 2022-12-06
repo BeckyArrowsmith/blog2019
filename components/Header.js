16100fd6f6e676a5d709b1aa6c029677dce9b43e
Becky Arrowsmith <becky.arrowsmith@googlemail.com>
2022-12-06 16:33:39 +0000

    Add LinkedIn

--- components/Header.js
+++ components/Header.js
@@ -9,7 +9,7 @@ const Header = () => (
 
             <p>Less code these days.</p>
             <p>Adventures in enabling the best out of people and engineering teams.</p>
-            <p><a href="http://twitter.com/beckycodes">Twitter</a> <a href="http://github.com/beckyarrowsmith">Github</a></p>
+            <p><a href="http://twitter.com/beckycodes">Twitter</a> <a href="https://www.linkedin.com/in/beckyarrowsmith/">LinkedIn</a> <a href="http://github.com/beckyarrowsmith">Github</a></p>
         </div>
     </header>
 );
