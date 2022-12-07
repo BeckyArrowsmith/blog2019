4bde0904d8008a6949c9b900ed64260507042370
BeckyArrowsmith <beckyarrowsmith@googlemail.com>
2022-12-07 19:07:51 +0000

    Update footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,26 +1,27 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 const Footer = () => (
-    <footer>
-        <div className="container">
-            <Link href="/about">
-                <a>About</a>
-            </Link>
-            <Link href="http://github.com/BeckyArrowsmith">
-                <a>GitHub</a>
-            </Link>
+  <footer>
+    <div className="container">
+      <Link>
+        <a href="http://twitter.com/beckycodes">Twitter</a>
+      </Link>
 
-            <Link href="https://github.com/BeckyArrowsmith/blog2019">
-                <a>Repository</a>
-            </Link>
+      <Link>
+        <a href="https://www.linkedin.com/in/beckyarrowsmith/">LinkedIn</a>
+      </Link>
 
-            <Link href="/pgp">
-                <a>PGP key</a>
-            </Link>
+      <Link href="http://github.com/BeckyArrowsmith">
+        <a>GitHub</a>
+      </Link>
 
-            <p>&copy; Becky Arrowsmith {new Date().getFullYear()} &#128029;</p>
-        </div>
-    </footer>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+
+      <p>&copy; Becky Arrowsmith {new Date().getFullYear()} &#128029;</p>
+    </div>
+  </footer>
 );
 
 export default Footer;
